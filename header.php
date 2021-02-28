63886c4b2f7d67c6b62d457b0fd1da29e12955ec
herorugby <hero.rugby1015@gmail.com>
2021-02-28 13:16:48 +0900

    assetsフォルダを用意し、cssファイルを投入しました

--- header.php
+++ header.php
@@ -7,6 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/contact-style.css">
+
+    <link href="contact-style.css" rel="stylesheet" media="screen">
+
     <?php wp_head(); ?>
 </head>
 
