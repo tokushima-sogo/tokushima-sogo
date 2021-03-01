9494016c7fa5a91734eab825e81341f1595fb3d0
hirotomo <hero.rugby1015@gmail.com>
2021-03-01 14:40:32 +0900

    テンプレファイルを変更しました

--- index.php
+++ index.php
@@ -7,35 +7,35 @@
     <!-- クイズゲームへ -->
     <a href="<?php echo esc_url(home_url('/quiz')); ?>"></a>
     <!-- 特集ページへ -->
-    <a href="<?php echo get_post_type_archive_link('special'); ?>"></a>
+    <a href="<?php echo get_post_type_archive_link('special'); ?>">特集ページへ</a>
 
     <!-- 5F -->
     <!-- 東部のアーカイブ -->
-    <a href="<?php echo get_post_type_archive_link('east'); ?>"></a>
+    <a href="<?php echo get_post_type_archive_link('east'); ?>">東部</a>
 
     <!-- 4F -->
     <!-- 西部のアーカイブ -->
-    <a href="<?php echo get_post_type_archive_link('west'); ?>"></a>
+    <a href="<?php echo get_post_type_archive_link('west'); ?>">西部</a>
 
     <!-- 3F -->
     <!-- 市内のアーカイブ -->
-    <a href="<?php echo get_post_type_archive_link('city'); ?>"></a>
+    <a href="<?php echo get_post_type_archive_link('city'); ?>">徳島市内</a>
 
     <!-- 2F -->
     <!-- 南部のアーカイブ -->
-    <a href="<?php echo get_post_type_archive_link('south'); ?>"></a>
+    <a href="<?php echo get_post_type_archive_link('south'); ?>">南部</a>
 
     <!-- 1F -->
     <!-- 徳島について固定ページ -->
-    <a href="<?php echo esc_url(home_url('/tokushima')); ?>"></a>
+    <a href="<?php echo esc_url(home_url('/tokushima')); ?>">徳島について</a>
     <!-- 名物のアーカイブ -->
-    <a href="<?php echo get_post_type_archive_link('famous'); ?>"></a>
+    <a href="<?php echo get_post_type_archive_link('famous'); ?>">名物</a>
     <!-- イベントのアーカイブ -->
-    <a href="<?php echo get_post_type_archive_link('event'); ?>"></a>
+    <a href="<?php echo get_post_type_archive_link('event'); ?>">イベント</a>
 
     <!-- B1F -->
     <!-- 都市伝説のアーカイブ -->
-    <a href="<?php echo get_post_type_archive_link('horror'); ?>"></a>
+    <a href="<?php echo get_post_type_archive_link('horror'); ?>">都市伝説</a>
 
 </main>
 
