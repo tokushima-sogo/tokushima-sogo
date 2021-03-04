14a1d9f53ff31efbbd89f0450b2cba9b80e44359
growhappyclover <growhappyclover@gmail.com>
2021-03-04 23:33:57 +0900

    wordpressって何でいらんことばっかりするかなあ……

--- page-quiz.php
+++ page-quiz.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
-
+<link href="<?php echo get_template_directory_uri(); ?>/assets/css/quiz.css" rel="stylesheet">
+<script>
+    const PATH = "<?php echo get_template_directory_uri(); ?>";
+</script>
 <main>
-    <p>ここはゲームエリア</p>
-</main>
+    <div id="start"></div>
+    <canvas id="canvas"></canvas>
 
+</main>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/quiz.js"></script>
 <?php get_footer(); ?>
