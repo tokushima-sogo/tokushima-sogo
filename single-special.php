0abd5f4d98d6c22ad1430da73427ca387a6a638e
herorugby <hero.rugby1015@gmail.com>
2021-03-09 00:38:37 +0900

    シングルページの修正

--- single-special.php
+++ single-special.php
@@ -1,13 +1,22 @@
 <?php get_header(); ?>
 
-<main>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/single-special.css">
 
+<main class="l-main">
 
-
-
-
-
-
+    <!-- breadCrumb -->
+    <div class="p-breadCrumb">
+        <div class="p-breadCrumb__inner">
+            <!-- breadcrumbループstart -->
+            <span>トップ</span>
+            <i class="fas fa-angle-right"></i>
+            <span>エリア名</span>
+            <i class="fas fa-angle-right"></i> <span>カテゴリ</span>
+            <i class="fas fa-angle-right"></i> <span>記事タイトル</span>
+            <!-- breadcrumbループend -->
+        </div>
+    </div>
+    <!-- /breadCrumb -->
 
     <!-- 投稿記事の出力 -->
     <?php if (have_posts()) : ?>
@@ -17,32 +26,41 @@
             <!-- タイトル -->
             <?php the_title(); ?>
 
+            <section class="l-description">
+                <div class="p-description u-center">
+                    <!-- 特集一覧のバナー -->
+                    <div class="c-specialBanner"><img src="./assets/images/bn_archive_special_ramen.png" alt="ラーメン特集"></div>
+                    <div class="p-description__item">
+                        <!-- 特集一覧のテキスト(概要？) -->
+                        <p class="c-description__text">
+                            徳島ラーメンと言えば！な、茶系×生卵な王道徳島ラーメンから、飲んだ後にもう一杯！汁まで飲み干せるあっさり〆のラーメンまで。みんな大好きラーメン特集！</p>
+                    </div>
+                </div>
+            </section>
+            <!-- /description -->
+
             <!-- 本文 -->
             <?php the_content(); ?>
 
-            <!-- サムネイルの表示 -->
-            <!-- <div class="pic">
-                <?php if (has_post_thumbnail()) : ?>
-                    <?php the_post_thumbnail('medium') ?>
-                <?php else : ?>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/noimage_600x400.png" alt="">
-                <?php endif; ?>
-            </div> -->
-
-            <!-- いいねぼたん -->
-            <?php echo do_shortcode('[wp_ulike]'); ?>
+            <!-- article -->
+            <section class="l-article">
+                <!-- ここから記事内容(投稿者の自由に) -->
+                <div class="c-img"><img src="./assets/images/takenoko1.jpg" alt=""></div>
+                <p class="c-text u-3times">いろいろ書きながら<span class="u-marker">ここをこう</span></p>
+                <p class="c-text u-bold">どんな感じかしらん</p>
+                <!-- 記事内容ここまで -->
+            </section>
+            <!-- /article -->
 
             <!-- タグを持ってくる -->
             <?php
             echo get_the_term_list($post->ID, 'taxotag', 'タグ:', '|');
             ?>
 
-            <!-- タグを持ってくる -->
-            <!-- <?php the_taxonomies(); ?> -->
-
         <?php endwhile; ?>
     <?php endif; ?>
 
 </main>
+<!-- /main -->
 
 <?php get_footer(); ?>
