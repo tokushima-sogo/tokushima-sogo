8d9e38a91610a32180faaa079174fb4af169ff71
herorugby <hero.rugby1015@gmail.com>
2021-03-09 23:41:47 +0900

    ばかになれ

--- taxonomy.php
+++ taxonomy.php
@@ -10,25 +10,57 @@
         <?php while (have_posts()) : ?>
             <?php the_post(); ?>
 
-            <!-- タイトルの表示 -->
-            <?php the_title(); ?>
+            <!-- articleList -->
+            <div class="p-articleList u-grid">
+                <!-- article -->
+                <article class="p-article c-more1">
+                    <!-- imgArea -->
+                    <div class="p-imgArea">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/flame_archive_common_thumbFlame.png" class="c-frame" alt="額縁">
+                        <!-- thumbnail -->
+                        <a href="<?php the_permalink(); ?>" class="c-thumbnail">
+                            <?php if (has_post_thumbnail()) : ?>
+                                <?php the_post_thumbnail('medium') ?>
+                            <?php else : ?>
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/noimage_600x400.png" alt="">
+                            <?php endif; ?>
+                        </a>
+                    </div>
+                    <!-- /imgArea -->
+                    <!-- textArea -->
+                    <div class="p-textArea">
+                        <div class="p-textContent u-flex">
+                            <div class="p-bookmark u-flex">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icon_archive_common_icon_heart02.png" class="c-icon__heart">
+                                <div class="c-bookmark__text"><span class="c-bookmark__count">10</span>いいね！</div>
+                            </div>
+                            <div class="p-tag u-flex">
+                                <div class="c-tag u-west u-mr15">
+                                    <?php
+                                    echo get_the_term_list($post->ID, 'area');
+                                    ?>
+                                </div>
+                                <div class="c-tag">
+                                    <?php
+                                    echo esc_html(get_post_type_object(get_post_type())->label);
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="c-title u-center"><?php the_title(); ?></div>
+                    </div>
+                    <!-- /textArea -->
+                </article>
 
-            <!-- サムネイルの表示 -->
-            <div class="pic">
-                <a href="<?php the_permalink(); ?>">
-                    <?php if (has_post_thumbnail()) : ?>
-                        <?php the_post_thumbnail('medium') ?>
-                    <?php else : ?>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/common/noimage_600x400.png" alt="">
-                    <?php endif; ?>
-                </a>
-            </div>
-
-            <a href="<?php the_permalink(); ?>">個別ページへ</a>
+                <!-- moreBtn -->
+                <div class="l-moreBtn u-right">
+                    <button class="c-moreBtn one u-center u-west">more</button>
+                </div>
+                <!-- /moreBtn -->
 
 
-        <?php endwhile; ?>
-    <?php endif; ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
 
 </main>
 
